6ed75f30ba2f93dd03bde8d47b890c299667c6c9
antho937 <anthony.saubaber93@gmail.com>
2016-04-14 23:06:55 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
 <div id="footer">
-<h1>FOOTER</h1>
+<h1><center>CONTACT US</center></h1>
 </div>
+<footer>
+	
+	<input type="text" name="nom" placeholder="Nom" size="30" maxlength="30" /><br>
+	<input type="text" name="prenom" placeholder="Prenom" size="30" maxlength="30" /><br>
+	<input type="text" name="mail" placeholder="Mail" size="30" maxlength="30" /><br>
+	<textarea type="text" name="Sujet" placeholder="Sujet" size="30" maxlength="300">
+	</textarea><br>
+	<INPUT TYPE="submit" NAME="Send" VALUE=" SEND "  class="envoyer">
+	
+	<div id="bas">COPYRIGHT UNCONAUT<br>51 RUE DU BERGER, 75001 PARIS</div>
+	
+</footer>
 </div>
 </body>
 </html>
+
